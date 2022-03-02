3556ea8943e911b5261a4cf678499211dbf24c0c
kaconk <harutonouciha@gmail.com>
2022-03-02 21:44:24 +0700

    async request

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import psycopg2
 from config.connection import config
-import requests
+#import requests
+import aiohttp
+import asyncio
 
 def getDevice():
     """ Connect to the PostgreSQL database server """
     conn = None
+    res = []
     try:
         # read connection parameters
         params = config()
@@ -17,8 +20,7 @@ def getDevice():
         rows = cur.fetchall()
         
         for row in rows:
-            print(row[0])
-            getData(row[0])
+            res.append(row[0])
         cur.close()
 	
     except (Exception, psycopg2.DatabaseError) as error:
@@ -26,13 +28,34 @@ def getDevice():
     finally:
         if conn is not None:
             conn.close()
-           
 
-def getData(addres):
-    req = requests.get('http://'+addres)
-    dt = req.json()
-    print(dt['temp'])
+    return res   
+
+async def getData(session, addres):
+    timeout = aiohttp.ClientTimeout(total=10)
+    try:
+        async with session.get(addres, timeout=timeout) as resp:
+            dt = await resp.json()
+            return dt
+    except Exception as e:
+        print(e)
+
+async def dataProc():
+
+    async with aiohttp.ClientSession() as session:
+        task = []
+        listDev = getDevice()
+
+        for dev in listDev:
+            addr = f'http://{dev}'
+            task.append(asyncio.ensure_future(getData(session,addr)))
+        
+        rawData = await asyncio.gather(*task)
+        #print(rawData)
+        if rawData is not None:
+            for res in rawData:
+                print(res['temp'])
 
 
 if __name__ == '__main__':
-    getDevice()
+    asyncio.run(dataProc())
